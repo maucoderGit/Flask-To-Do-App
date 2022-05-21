9de44c047cae9b178af803ace73c6293e8aa0100
Mauricio Gonzalez <dreg5000.bbb@gmail.com>
2022-05-21 12:41:17 -0400

    Foundaments

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 # Flask
-from flask import Flask
+from flask import Flask, Response, escape, make_response, redirect, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello world flask'
+def index() -> Response:
+    user_ip: str | None = request.remote_addr
+
+    response: Response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+@app.route('/hello')
+def home() -> str:
+    user_ip = request.cookies.get('user_ip')
+    user_ip = escape(user_ip)
+
+    return f'Hello World, This IP is {user_ip}'
+
+
+if __name__ == '__main__':
+    app.run(port = 5000, debug = True)
