75ad729b547b6c5f2283a40f693efdb977b45029
Mauricio Gonzalez <dreg5000.bbb@gmail.com>
2022-05-29 13:06:37 -0400

    Firestore

--- flask_test.sh
+++ flask_test.sh
@@ -6,6 +6,6 @@ pip install -r requirements.txt
 export FLASK_APP=main.py
 export FLASK_DEBUG=1
 export FLASK_ENV=development
-export GOOGLE_CLOUD_PROJECT='my-flask-to-do-list'
+export GOOGLE_CLOUD_PROJECT='./firebase_admin.json'
 
 flask run
