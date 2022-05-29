91b74adaeb5daa1a74b592b87211a0087996ee3d
Mauricio Gonzalez <dreg5000.bbb@gmail.com>
2022-05-29 12:24:09 -0400

    Adding FireBase Service

--- app/config.py
+++ app/config.py
@@ -5,18 +5,18 @@ from decouple import config
 class Config:
     SECRET_KEY = urandom(40)
     PRESERVE_CONTEXT_ON_EXCEPTION = False
+    FLASK_APP="main.py"
+    GOOGLE_CLOUD_PROJECT='my-flask-to-do-list'
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    FLASK_APP="main.py"
     FLASK_DEBUG=1
-    GOOGLE_CLOUD_PROJECT='my-flask-to-do-list'
+    
 
 class ProductionConfig(Config):
     DEBUG = False
     FLASK_DEBUG=0
-    GOOGLE_CLOUD_PROJECT='my-flask-to-do-list'
 
 config = {
     'development': DevelopmentConfig,
